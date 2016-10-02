91df5f961a09c11f0f3d8d77fe7cb32c10fab177
qihouliang <qihouliang15@mails.ucas.ac.cn>
2016-10-02 21:32:49 +0800

    comit

---
+++ 1.1.2/src/main/java/ict/ocrabase/main/java/query/admaster/prasedata/Ads.java
@@ -0,0 +1,356 @@
+package ict.ocrabase.main.java.query.admaster.prasedata;
+
+public class Ads {
+	private String idfv;
+	private String screenpix;
+	private String is_root;
+	private String mac1;
+	private String mcc;
+	private String appid;
+	private String city;
+	private String time;
+	private String event;
+	private String province;
+	private String gender;
+	private String rawModel;
+	private String os;
+	private String phonenumber;
+	private String osv;
+	private String imei;
+	private String androidID;
+	private String cid;
+	private String country;
+	private String cookie;
+	private String appv;
+	private String sdkv;
+	private String distinctID;
+	private String dim_6;
+	private String dim_5;
+	private String email;
+	private String month;
+	private String ap_mac;
+	private String idfa;
+	private String model;
+	private String events;
+	private String dim_7;
+	private String mac;
+	private String date;
+	private String net;
+	private String isAnonymous;
+	private String timestamp;
+	private String id;
+	private String ad_tracked;
+	private String name;
+	private String mnc;
+	private String svrTime;
+	private String package_name;
+	private String hour;
+	private String operator;
+	private String ip;
+	private String OpenUDID;
+	private String brand;
+	private String rawDistinctID;
+	private String week;
+	public String getIdfv() {
+		return idfv;
+	}
+	public void setIdfv(String idfv) {
+		this.idfv = idfv;
+	}
+	public String getScreenpix() {
+		return screenpix;
+	}
+	public void setScreenpix(String screenpix) {
+		this.screenpix = screenpix;
+	}
+	public String getIs_root() {
+		return is_root;
+	}
+	public void setIs_root(String is_root) {
+		this.is_root = is_root;
+	}
+	public String getMac1() {
+		return mac1;
+	}
+	public void setMac1(String mac1) {
+		this.mac1 = mac1;
+	}
+	public String getMcc() {
+		return mcc;
+	}
+	public void setMcc(String mcc) {
+		this.mcc = mcc;
+	}
+	public String getAppid() {
+		return appid;
+	}
+	public void setAppid(String appid) {
+		this.appid = appid;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getEvent() {
+		return event;
+	}
+	public void setEvent(String event) {
+		this.event = event;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getRawModel() {
+		return rawModel;
+	}
+	public void setRawModel(String rawModel) {
+		this.rawModel = rawModel;
+	}
+	public String getOs() {
+		return os;
+	}
+	public void setOs(String os) {
+		this.os = os;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getOsv() {
+		return osv;
+	}
+	public void setOsv(String osv) {
+		this.osv = osv;
+	}
+	public String getImei() {
+		return imei;
+	}
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+	public String getAndroidID() {
+		return androidID;
+	}
+	public void setAndroidID(String androidID) {
+		this.androidID = androidID;
+	}
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCookie() {
+		return cookie;
+	}
+	public void setCookie(String cookie) {
+		this.cookie = cookie;
+	}
+	public String getAppv() {
+		return appv;
+	}
+	public void setAppv(String appv) {
+		this.appv = appv;
+	}
+	public String getSdkv() {
+		return sdkv;
+	}
+	public void setSdkv(String sdkv) {
+		this.sdkv = sdkv;
+	}
+	public String getDistinctID() {
+		return distinctID;
+	}
+	public void setDistinctID(String distinctID) {
+		this.distinctID = distinctID;
+	}
+	public String getDim_6() {
+		return dim_6;
+	}
+	public void setDim_6(String dim_6) {
+		this.dim_6 = dim_6;
+	}
+	public String getDim_5() {
+		return dim_5;
+	}
+	public void setDim_5(String dim_5) {
+		this.dim_5 = dim_5;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
+	public String getAp_mac() {
+		return ap_mac;
+	}
+	public void setAp_mac(String ap_mac) {
+		this.ap_mac = ap_mac;
+	}
+	public String getIdfa() {
+		return idfa;
+	}
+	public void setIdfa(String idfa) {
+		this.idfa = idfa;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getEvents() {
+		return events;
+	}
+	public void setEvents(String events) {
+		this.events = events;
+	}
+	public String getDim_7() {
+		return dim_7;
+	}
+	public void setDim_7(String dim_7) {
+		this.dim_7 = dim_7;
+	}
+	public String getMac() {
+		return mac;
+	}
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getNet() {
+		return net;
+	}
+	public void setNet(String net) {
+		this.net = net;
+	}
+	public String getIsAnonymous() {
+		return isAnonymous;
+	}
+	public void setIsAnonymous(String isAnonymous) {
+		this.isAnonymous = isAnonymous;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getAd_tracked() {
+		return ad_tracked;
+	}
+	public void setAd_tracked(String ad_tracked) {
+		this.ad_tracked = ad_tracked;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMnc() {
+		return mnc;
+	}
+	public void setMnc(String mnc) {
+		this.mnc = mnc;
+	}
+	public String getSvrTime() {
+		return svrTime;
+	}
+	public void setSvrTime(String svrTime) {
+		this.svrTime = svrTime;
+	}
+	public String getPackage_name() {
+		return package_name;
+	}
+	public void setPackage_name(String package_name) {
+		this.package_name = package_name;
+	}
+	public String getHour() {
+		return hour;
+	}
+	public void setHour(String hour) {
+		this.hour = hour;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getOpenUDID() {
+		return OpenUDID;
+	}
+	public void setOpenUDID(String openUDID) {
+		OpenUDID = openUDID;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getRawDistinctID() {
+		return rawDistinctID;
+	}
+	public void setRawDistinctID(String rawDistinctID) {
+		this.rawDistinctID = rawDistinctID;
+	}
+	public String getWeek() {
+		return week;
+	}
+	public void setWeek(String week) {
+		this.week = week;
+	}
+	
+	
+}
---
+++ 1.1.2/src/main/java/ict/ocrabase/main/java/query/admaster/prasedata/Json2file.java
@@ -0,0 +1,18 @@
+package ict.ocrabase.main.java.query.admaster.prasedata;
+
+import com.google.gson.Gson;
+
+public class Json2file {
+	private Gson gson;
+	public Json2file(){
+		
+	}
+	
+	public Ads json2Ads(String str){
+		Gson gson = new Gson();
+		Ads ads = 
+		
+		
+	}
+	
+}
